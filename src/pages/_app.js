30263081a7c98ff4eee057b4ad2806b9813f7be8
Federico <nvfede@gmail.com>
2022-12-02 12:47:31 +0100

    first

---
+++ src/pages/_app.js
@@ -0,0 +1,14 @@
+import MainLayout from '@layout/MainLayout';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </>
+  );
+}
+
+export default MyApp;
